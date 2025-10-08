7ad7b4d558607c959512da851b49807e20010bad
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2025-10-08 17:51:04 -0300

    For and Do While

--- Program.cs
+++ Program.cs
@@ -1,11 +1,116 @@
 ﻿using ExemplosFundamentosDotNET.Models;
 
-int numero = 5;
 
-for (int contador = 0; contador <= 10; contador++)
+string opcao;
+bool exibirMenu = true;
+while (exibirMenu)
 {
-    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    Console.WriteLine("digite sua opção: ");
+    Console.WriteLine("1 - Cadastro de cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente ");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+        case "2":
+            Console.WriteLine("Buscar cliente");
+            break;
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+
+            // Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opção Inválida");
+            break;
+
+    }
 }
+
+Console.WriteLine("Programa encerrado");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int soma = 0, numero = 0;
+
+
+
+// do
+// {
+//     Console.WriteLine("Digite um numero (digite 0 para parar): ");
+//     numero = Convert.ToInt32(Console.ReadLine());
+
+//     soma += numero;
+
+// } while (numero != 0);
+
+// Console.WriteLine($"Total de numeros digitados é: {soma}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+// int contador = 1;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{contador}º Execução: {numero} x {contador} =  {numero * contador}");
+//     contador++;
+//     if (contador == 6)
+//     {
+
+//         break;
+//     }
+// }
+
+
+
+
+
+
+
+
+// int numero = 5;
+
+// for (int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
 
 
 
@@ -48,13 +153,9 @@ for (int contador = 0; contador <= 10; contador++)
 // calc.RaizQuadrada(9);
 
 
-
-
-
-
-
-
 //not operator
+
+
 // bool choveu = true;
 // bool estaTarde = false;
 
